f9d2c16c829dcc9f00508d40916feb9b94994ac8
Arjun <arjun@dell-15.localdomain>
2020-04-21 21:49:54 +0530

    Added clear

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var coordinates = [];
 var canvas;
 var database;
+var drawing = [];
 
 function setup() {
     canvas = createCanvas(800, 400);
@@ -8,13 +9,17 @@ function setup() {
 }
 
 function draw() {
-    sync();
+    document.getElementById('clear1').onclick = () => {
+        background("white");
+        database.ref('array').set(0);
+    }
 }
 
 function mouseDragged() {
     coordinates.push([mouseX, mouseY]);
-    database.ref('array').set(coordinates);
+    drawing = coordinates;
     for (var i = 0; i < coordinates.length; i++) {
+        //console.log(drawing)
         var x = coordinates[i][0];
         var y = coordinates[i][1];
         //console.log(i);
@@ -22,26 +27,23 @@ function mouseDragged() {
             line(x, y, coordinates[i - 1][0], coordinates[i - 1][1])
         }
     }
+
+    var array = database.ref('array');
+    var array2;
+    array.on("value", (data) => {
+        array2 = data.val();
+    })
+    //console.log(array2);
+    if (array2 == 0) {
+        array2 = [];
+        //console.log('done');
+    }
+    var array3 = [...array2, ...drawing];
+    //console.log(array3);
+    database.ref('array').set(array3);
     endShape();
 }
 
 function mouseReleased() {
     coordinates = [];
-}
-
-function sync() {
-    var array;
-    var drawCoords = database.ref('array');
-    drawCoords.on('value', function (data) {
-        array = data.val()
-    })
-    console.log(array);
-    for (var i = 0; i < array.length; i++) {
-        var x = array;
-        var y = array;
-        //console.log(i);
-        if (i >= 1) {
-            line(x, y, array[i - 1][0], array[i - 1][1])
-        }
-    }
 }
